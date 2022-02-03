492cfed5742d8c75abf0251eda8aa7539ddbd78f
Justin <Justin.nthai@gmail.com>
2022-02-03 12:33:52 -0800

    Set up loop for 5 rounds and track respective scores, Return final score and message, Adjust plan logic to take into account a possibility of final draw given round restraint

--- js/rps.js
+++ js/rps.js
@@ -1,58 +1,64 @@
-// Plan
+/* Plan
 
-/*
-
-Ask user to choose rock, paper, or scissors. (make sure the string is case insensitive)
-and make sure their selection is valid
+Ask user to choose rock, paper, or scissors (make sure the string is case insensitive
+and their selection is valid).
 
 Have the computer randomly choose rock, paper, or scissors.
 
-compare the answers of the user input and the computer. and output the winner of the round.
+Compare the answers of the user input and the computer; output user round result. 
 
-*/
+Play 5 rounds and keep score and report the winner/loser at the end. Take draws into account--
+Given restraints, there will be chances of no final winner/loser */
 
-// let playerSelection = prompt("Rock, Paper, or Scissors? Choose your fate."); 
-// while (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !=="paper" && playerSelection.toLowerCase() !=="scissors")
-//   {
-//     alert(playerSelection + ' is not a valid option.');
-//     playerSelection = prompt("Rock, Paper, or Scissors? Choose your fate.");
-//   };
+let userScore = 0;
+let computerScore = 0;
 
-function computerPlay() {
-  const computerOptions = ['rock', 'paper', 'scissors'];
-  const random = Math.floor(Math.random() * computerOptions.length);
-  return computerOptions[random];
+function game() {
+  for (let round = 1; round <=5; round++) {
+
+    let playerSelection = prompt("Rock, Paper, or Scissors? Choose your fate."); 
+    while (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !=="paper" && playerSelection.toLowerCase() !=="scissors")
+      {
+        alert(`${playerSelection} is not a valid option.`);
+        playerSelection = prompt("Rock, Paper, or Scissors? Choose your fate.");
+      };
+
+    function computerPlay() {
+      const computerOptions = ['rock', 'paper', 'scissors'];
+      const computerRandom = Math.floor(Math.random() * computerOptions.length);
+      return computerOptions[computerRandom];
+    }
+
+    function playRound(playerSelection, computerSelection) {
+      if (playerSelection === computerSelection) {
+            return 'It\'s a Draw!';
+          } else if ((playerSelection === 'rock' && computerSelection === 'paper') || 
+            (playerSelection === 'paper' && computerSelection === 'scissors') || 
+            (playerSelection === 'scissors' && computerSelection === 'rock')) {
+            computerScore++;
+            return 'You Lose! ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) +
+            ' beats ' + playerSelection.charAt(0) + playerSelection.slice(1) + '!';
+          } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
+            (playerSelection === 'paper' && computerSelection === 'rock') || 
+            (playerSelection === 'scissors' && computerSelection === 'paper')) {
+            userScore++;
+            return 'You Win! ' + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) +
+            ' beats ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+          } 
+    }
+
+    const computerSelection = computerPlay();
+
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerSelection, computerSelection);
+    console.log(userScore, computerScore)
+  }
+
+  if (userScore > computerScore) {
+    return `The final score out of 5 rounds is User: ${userScore} - Computer: ${computerScore}. You Win!`;
+  } else if (userScore < computerScore) {
+    return `The final score out of 5 rounds is User: ${userScore} - Computer: ${computerScore}. You Lose!`;
+  } else if (userScore === computerScore) {
+    return `The final score out of 5 rounds is User: ${userScore} - Computer: ${computerScore}. It's a Draw!`;
+  }
 }
-
-// const computerSelection = computerPlay();
-
-// function rps(playerSelection, computerSelection) {
-//   if (playerSelection === computerSelection) {
-//     return 'It\'s a Draw!';
-//   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-//     return 'You Lose! Paper beats Rock!';
-//   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-//     return 'You Win! Paper beats Rock!';
-//   }
-// };
- 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-        return 'It\'s a Draw!';
-      } else if ((playerSelection === 'rock' && computerSelection === 'paper') || 
-        (playerSelection === 'paper' && computerSelection === 'scissors') || 
-        (playerSelection === 'scissors' && computerSelection === 'rock')) {
-        return 'You Lose! ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) +
-        ' beats ' + playerSelection.charAt(0) + playerSelection.slice(1) + '!';
-      } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
-        (playerSelection === 'paper' && computerSelection === 'rock') || 
-        (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        return 'You Win! ' + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) +
-        ' beats ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-      } 
-}
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerSelection, computerSelection);
