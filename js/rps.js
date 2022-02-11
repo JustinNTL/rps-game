b7d176ba6235111aac4f879b155a738030fa2986
Justin <Justin.nthai@gmail.com>
2022-02-10 16:18:44 -0800

    Move script to head instead with defer in index file, Remove 5 round rps logic in js

--- js/rps.js
+++ js/rps.js
@@ -1,27 +1,19 @@
 /* Plan
 
 Ask user to choose rock, paper, or scissors (make sure the string is case insensitive
-and their selection is valid).
+and their selection is valid)
 
-Have the computer randomly choose rock, paper, or scissors.
+Have the computer randomly choose rock, paper, or scissors
 
-Compare the answers of the user input and the computer; output user round result. 
+Compare the answers of the user input and the computer; output user round result 
 
 Play 5 rounds and keep score and report the winner/loser at the end. Take draws into account--
-Given restraints, there will be chances of no final winner/loser */
+Given restraints, there will be chances of no final winner/loser 
+
+Edit: Use buttons instead of asking for a string */
 
 let userScore = 0;
 let computerScore = 0;
-
-function game() {
-  for (let round = 1; round <=5; round++) {
-
-    let playerSelection = prompt("Rock, Paper, or Scissors? Choose your fate."); 
-    while (playerSelection.toLowerCase() !== "rock" && playerSelection.toLowerCase() !=="paper" && playerSelection.toLowerCase() !=="scissors")
-      {
-        alert(`${playerSelection} is not a valid option.`);
-        playerSelection = prompt("Rock, Paper, or Scissors? Choose your fate.");
-      };
 
     function computerPlay() {
       const computerOptions = ['rock', 'paper', 'scissors'];
@@ -48,11 +40,11 @@ function game() {
             ' beats ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
       } 
     }
-
+  
     console.log(playRound(playerSelection, computerSelection));
     console.log(playerSelection, computerSelection);
-    console.log(userScore, computerScore)
-  }
+    console.log(userScore, computerScore);
+  
 
   if (userScore > computerScore) {
     return `The final score out of 5 rounds is User: ${userScore} - Computer: ${computerScore}. You Win!`;
@@ -61,4 +53,3 @@ function game() {
   } else if (userScore === computerScore) {
     return `The final score out of 5 rounds is User: ${userScore} - Computer: ${computerScore}. It's a Draw!`;
   }
-}
